7fbbe62f324b1e1b5641099db6432e957438e8f9
Alessandro Calista <alessandromaria.calista@gmail.com>
2025-11-14 15:52:49 +0100

    Add Flask app and User model; update .gitignore

---
+++ backend/model/user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, name, surname, username, email, password):
+        self.name = name
+        self.surname = surname
+        self.username = username
+        self.email = email
+
+
+
